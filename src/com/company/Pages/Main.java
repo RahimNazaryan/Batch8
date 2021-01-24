4af5c636b264c18d54ec71be57c0722942dedce4
unknown <nazaryanrahim@gmail.com>
2021-01-24 12:06:14 -0500

    first commit

---
+++ src/com/company/Pages/Main.java
@@ -0,0 +1,10 @@
+package com.company.Pages;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println("Hello Batch8");
+    }
+}
